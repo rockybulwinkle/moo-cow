705e6ee32d7dd8df096a8b4e8bdacc401e277806
Chris Hopwood <rockybulwinkle@gmail.com>
2014-02-11 15:59:02 +0000

    hopefully working capture, training, and testing

--- capture.py
+++ capture.py
@@ -4,10 +4,19 @@
 
 import subprocess
 import os
+import atexit
+import time
+import build_net
+from pybrain.datasets            import SequentialDataSet
 
-
-WII_DATA_FILENAME = "wii_data"
+WII_DATA_FILENAME = "wiidata"
 WII_CAPTURE = "wiiuse/wii_capture"
+
+def signal_handler():
+    print "exiting"
+    close_c_code()
+    exit(0)
+
 def mkfifo():
     os.system("mkfifo %s"%WII_DATA_FILENAME)
 
@@ -22,8 +31,50 @@ def close_c_code():
     os.system("kill `pidof %s`"%WII_CAPTURE)
     rmfifo()
 
+def get_samples():
+    samples = SequentialDataSet(6, 2)
+    num_samples = 0
+    while True:
+        with open(WII_DATA_FILENAME,"r") as f:
+            msg = f.read().split()
 
-launch_c_code()
-import time
-time.sleep(10000000)
-close_c_code()
+        if msg[0] is "start":
+            print "recording data"
+            samples.newSequence()
+        elif msg[0] is "stop":
+            print "done recording data, waiting"
+            if num_samples > 10:
+                break
+            num_samples += 1
+        elif len(msg) > 1:
+            print msg
+            data = map(float, msg)[0:6]
+            samples.appendLinked(tuple(data), (0,1) if num_samples < 5 else (1,0))
+
+    return samples
+
+def test_net(net):
+    while True:
+        with open(WII_DATA_FILENAME,"r") as f:
+            msg = f.read().split()
+
+        if msg[0] is "start":
+            print "recording data"
+        elif msg[0] is "stop":
+            print "done recording data, waiting"
+        elif len(msg) > 1:
+            data = map(float, msg)[0:6]
+            net.activate(data)
+
+   
+
+if __name__=="__main__":
+    atexit.register(signal_handler)
+    launch_c_code() 
+    net = build_net.build_network(6, 10, 2)
+    samples = get_samples()
+    print "training"
+    build_net.train(net, samples)
+    print "done training, now enter test data"
+    test_net(net)
+    close_c_code()
