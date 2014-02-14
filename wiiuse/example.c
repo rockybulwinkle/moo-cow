88ea3685016e8b47f3e1b4fba45deaaa70b26010
fendrirj <fendrirj@rose-hulman.edu>
2014-02-14 12:42:03 -0500

    Removed unused or unneeded code, added some comments, removed some comments

--- wiiuse/example.c
+++ wiiuse/example.c
@@ -1,48 +1,27 @@
-
-
-/**
- *	@file
- *
- *	@brief Example using the wiiuse API.
- *
- *	This file is an example of how to use the wiiuse library.
- */
-
-#include <stdio.h>                      /* for printf */
-
-#include "wiiuse.h"                     /* for wiimote_t, classic_ctrl_t, etc */
+#include <stdio.h>                      
+#include "wiiuse.h"                     
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <string.h>
-
-#ifndef WIIUSE_WIN32
-#include <unistd.h>                     /* for usleep */
-#endif
+#include <unistd.h>                     
 
 #define MAX_WIIMOTES				1
 #define PIPE					"wiidata"
 #define SAMPLES					"samples"
 
 
-/**
- *	@brief Callback that handles an event.
- *
- *	@param wm		Pointer to a wiimote_t structure.
- *
- *	This function is called automatically by the wiiuse library when an
- */
 void handle_event(struct wiimote_t* wm, char * save_path, int * num_samples, int mode) {	
 	static int prev_state = 0;
-	static int current_state = 0;
-	int fd;
-	char message[50] = "";
+	static int current_state = 0; //State of 1 means gesture data is currently being collected
+                                  //State of 0 means gesture data is not currently being collected
+	int fd; //Holds file descriptor for pipe
+	char message[50] = ""; //Holds data to send through pipe
 
-	/* if a button is pressed, report it */
-	if (IS_PRESSED(wm, WIIMOTE_BUTTON_B)) {
+	if (IS_PRESSED(wm, WIIMOTE_BUTTON_B)) { //Detects the b button being pressed
 		current_state = 1;	
-		if(!(wm->exp.type == EXP_NUNCHUK)){
+		if(!(wm->exp.type == EXP_NUNCHUK)){ //If motion_plus isn't set, set both it and motion sensing
 			wiiuse_set_motion_plus(wm, 1);
 			wiiuse_motion_sensing(wm, 1);
 		}
@@ -56,42 +35,42 @@ void handle_event(struct wiimote_t* wm, char * save_path, int * num_samples, int
 			fd = open(PIPE, O_WRONLY);
 			write(fd, message, strlen(message) +1);
 			close(fd);
-			printf("%s\n", message);
 		}else{//print to file
 			FILE * fp;
 			char filepath[100]="";
-			sprintf(filepath,"%s%d",save_path, *num_samples);
+			sprintf(filepath,"%s%d",save_path, *num_samples); //Create path to file for current training sample
 			fp = fopen(filepath, "ab+");
 			fprintf(fp, "%d %d %d %d %d %d\n",
 				wm->accel.x, wm->accel.y, wm->accel.z,
 				wm->exp.mp.raw_gyro.pitch, wm->exp.mp.raw_gyro.roll,
 				wm->exp.mp.raw_gyro.yaw);
 			fclose(fp);
-			printf( "%d %d %d %d %d %d\n",
-				wm->accel.x, wm->accel.y, wm->accel.z,
-				wm->exp.mp.raw_gyro.pitch, wm->exp.mp.raw_gyro.roll,
-				wm->exp.mp.raw_gyro.yaw);		
+		
 		}
-
-				
+		printf( "%d %d %d %d %d %d\n",
+			wm->accel.x, wm->accel.y, wm->accel.z,
+			wm->exp.mp.raw_gyro.pitch, wm->exp.mp.raw_gyro.roll,
+			wm->exp.mp.raw_gyro.yaw);
+        				
 	} else{
-		current_state=0;
+		current_state=0; //B is not currently being pressed
 	}
 
-	if(current_state != prev_state){
+	if(current_state != prev_state){ //If b went from being pressed to not or vice versa
 		if(current_state){
-			if(mode ==0){
+			if(mode ==0){ //Indicate that data transmission has started
 				fd = open(PIPE, O_WRONLY);
 				write(fd, "start\n", strlen("start\n") +1);
 				close(fd);
 			}		
 		}else{
-			if(mode==0){
+			if(mode==0){ //Indicate that data transmission has stopped
 				fd = open(PIPE, O_WRONLY);
 				write(fd, "stop\n", strlen("stop\n") +1);
 				close(fd);
 			}else{
-				*num_samples = *num_samples + 1;
+				*num_samples = *num_samples + 1; //If the b button was released,
+                                                 //increment the num of training samples
 			}
 		}
 	}
@@ -99,41 +78,6 @@ void handle_event(struct wiimote_t* wm, char * save_path, int * num_samples, int
 	
 }
 
-
-/**
- *	@brief Callback that handles a disconnection event.
- *
- *	@param wm				Pointer to a wiimote_t structure.
- *
- *	This can happen if the POWER button is pressed, or
- *	if the connection is interrupted.
- */
-void handle_disconnect(wiimote* wm) {
-	printf("\n\n--- DISCONNECTED [wiimote id %i] ---\n", wm->unid);
-}
-
-short any_wiimote_connected(wiimote** wm, int wiimotes) {
-	int i;
-	if (!wm) {
-		return 0;
-	}
-
-	for (i = 0; i < wiimotes; i++) {
-		if (wm[i] && WIIMOTE_IS_CONNECTED(wm[i])) {
-			return 1;
-		}
-	}
-
-	return 0;
-}
-
-
-/**
- *	@brief main()
- *
- *	Connect to up to two wiimotes and print any events
- *	that occur on either device.
- */
 int main(int argc, char** argv) {
 	wiimote** wiimotes;
 	int found, connected, mode, commandType, num_samples;
@@ -209,18 +153,12 @@ int main(int argc, char** argv) {
 	wiiuse_rumble(wiimotes[0], 0);
 	
 	wiiuse_set_flags(wiimotes[0], WIIUSE_CONTINUOUS,0 );
-	while (any_wiimote_connected(wiimotes, MAX_WIIMOTES)) {
+	while (WIIMOTE_IS_CONNECTED(wiimotes[0])) {
 		if (wiiuse_poll(wiimotes, MAX_WIIMOTES)) {
-
 			switch (wiimotes[0]->event) {
 				case WIIUSE_EVENT:
 					/* a generic event occurred */
 					handle_event(wiimotes[0], save_path, &num_samples, mode);
-					break;
-				case WIIUSE_DISCONNECT:
-				case WIIUSE_UNEXPECTED_DISCONNECT:
-					/* the wiimote disconnected */
-					handle_disconnect(wiimotes[0]);
 					break;
 				default:
 					break;
