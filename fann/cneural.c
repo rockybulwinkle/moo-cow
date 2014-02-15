08c90c4817a0afd311a90e0d9d816addccb2f526
Chris Hopwood <rockybulwinkle@gmail.com>
2014-02-15 15:53:21 +0000

    trained net and samples

--- fann/cneural.c
+++ fann/cneural.c
@@ -1,28 +1,27 @@
 #include "fann.h"
-#include <math.h>
-
-#define NUM_INPUTS 6
-#define NUM_OUTPUTS 4
-#define NUM_LAYERS 3
-#define NUM_NEURONS 3
 
 int main()
 {
-
+    const unsigned int num_input = 6;
+    const unsigned int num_output = 4;
+    const unsigned int num_layers = 3;
+    const unsigned int num_neurons_hidden = 100;
     const float desired_error = (const float) 0.001;
     const unsigned int max_epochs = 500000;
     const unsigned int epochs_between_reports = 1000;
 
-    struct fann *ann = fann_create_standard(NUM_LAYERS, NUM_INPUTS,
-        NUM_NEURONS, NUM_OUTPUTS);
+    struct fann *ann = fann_create_standard(num_layers, num_input,
+        num_neurons_hidden, num_output);
+
+    fann_set_learning_rate(ann, .001);
 
     fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
     fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
 
-    fann_train_on_file(ann, "xor.data", max_epochs,
+    fann_train_on_file(ann, "../training_file", max_epochs,
         epochs_between_reports, desired_error);
 
-    fann_save(ann, "xor_float.net");
+    fann_save(ann, "wii.net");
 
     fann_destroy(ann);
 
