8932d1c6655daf0b85aed008da883b31e6803be9
Chris Hopwood <rockybulwinkle@gmail.com>
2014-02-15 15:58:34 +0000

    removed unneeded python files for fann

--- globals.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python2
-# vim:tabstop=4:shiftwidth=4:smarttab:expandtab:softtabstop=4:autoindent:
-
-WII_DATA_FILENAME = "wiidata"
-WII_CAPTURE = "wiiuse/wii_capture"
-SIMPLE_SAMPLES_DIR = "wiiuse/samples/simple"
-NUM_INPUTS = 6
