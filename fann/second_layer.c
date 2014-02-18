6ff52b7dfd41d6d7abaf7eb6559e03ce5bdd9889
Chris Hopwood <rockybulwinkle@gmail.com>
2014-02-18 16:04:12 +0000

    second layer and first layer networks

--- fann/second_layer.c
+++ fann/second_layer.c
@@ -8,7 +8,7 @@ int main()
     const unsigned int num_neurons_hidden = 100;
     const float desired_error = (const float) 0.001;
     const unsigned int max_epochs = 50000;
-    const unsigned int epochs_between_reports = 1000;
+    const unsigned int epochs_between_reports = 1;
 
     struct fann *ann = fann_create_standard(num_layers, num_input,
         num_neurons_hidden, num_output);
