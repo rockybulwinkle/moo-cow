9d73df0d75beaff80868fccf41fcae22d8294a7a
Chris Hopwood <rockybulwinkle@gmail.com>
2014-02-18 15:35:08 +0000

    fixed python and c code to create complex training file

--- complex_make_training_file.py
+++ complex_make_training_file.py
@@ -6,7 +6,7 @@ this file creates a training file for FANN based on the data created by wiiuse/c
 import load_samples
 import os
 
-samples = load_samples.load_samples(get_file_name_only = True, input_directory="wiiuse/samples/simple/")
+samples = load_samples.load_samples(get_file_name_only = True, input_directory="samples/")
 new_samples = dict()
 for key in sorted(samples.keys()):
     joined_samples = []
@@ -22,7 +22,7 @@ output_values = ["1 0", "0 1"]
 
 f = open("complex_training_file", "w")
 
-f.write("%d 4 %d\n"%(num_samples, len(output_values)))
+f.write("%d 400 %d\n"%(num_samples, len(output_values)))
 
 for idx, key in enumerate(sorted(new_samples.keys())):
     for sample in  new_samples[key]:
@@ -31,9 +31,19 @@ for idx, key in enumerate(sorted(new_samples.keys())):
             for line in input_file:
                 input_length += 1
 
+        print input_length
         os.system("./resample %s %d %s %d"%(sample, input_length, "/tmp/wii_out_file", 100))
         with open("/tmp/wii_out_file", "r") as tmp_file:
-            f.write(" ".join(map(str, list(tmp_file))).replace("\n", ""))
+            for line in tmp_file:
+                line= line.replace("\n","")
+                line = line.split()
+                line = map(float, line)
+                max_ = max(line)
+                line = map(lambda x: 1.0 if x == max_ else 0.0, line)
+                line = map(str, line)
+                line = " ".join(line)
+                f.write(line+" ")
+                
         #f.write(" ".join(map(str, sample)))
         f.write("\n")
         f.write(output_values[idx])
--- resample.c
+++ resample.c
@@ -19,7 +19,8 @@ int main(int argc, char * argv[]){
 	src_data.data_in = malloc(sizeof(float)*in_size*4);
 	
 	for (i = 0; i < in_size*4; i+=4){
-		fscanf(in_file, "%f %f %f %f\n", &src_data.data_in[i], &src_data.data_in[i+1], &src_data.data_in[i+2], &src_data.data_in[i+3]);
+		fscanf(in_file, "%f %f %f %f \n", &src_data.data_in[i], &src_data.data_in[i+1], &src_data.data_in[i+2], &src_data.data_in[i+3]);
+		//printf("%f %f %f %f\n", src_data.data_in[i], src_data.data_in[i+1], src_data.data_in[i+2], src_data.data_in[i+3]);
 	}
 	fclose(in_file);
 
@@ -27,12 +28,13 @@ int main(int argc, char * argv[]){
 
 	src_data.input_frames = in_size;
 	src_data.output_frames = out_size;
-	src_data.src_ratio = out_size/in_size;
+	src_data.src_ratio = out_size/((float)in_size);
 
 	src_simple(&src_data, 4, 4);
 	FILE * out_file = fopen(argv[3], "w");
 	for (i = 0; i < out_size * 4; i += 4){
 		fprintf(out_file,"%f %f %f %f\n", src_data.data_out[i], src_data.data_out[i+1], src_data.data_out[i+2], src_data.data_out[i+3]);
+		//printf("%f %f %f %f\n", src_data.data_out[i], src_data.data_out[i+1], src_data.data_out[i+2], src_data.data_out[i+3]);
 	}
 
 }
