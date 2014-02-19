c389bc35ee2f712cfc802399c6fb6829fec88b1a
Chris Hopwood <rockybulwinkle@gmail.com>
2014-02-19 16:11:24 +0000

    reverted run_network.c

--- run_network.c
+++ run_network.c
@@ -3,80 +3,63 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <string.h>
-#include <samplerate.h>
 #include "fann.h"
 
 #define PATH_TO_WII_PIPE "wiiuse/wiidata"
 #define PATH_TO_NETWORK "fann/wii.net"
-#define PATH_TO_SECOND_LAYER "fann/second_layer_wii.net"
 #define PATH_TO_SAMPLES "wiiuse/samples/advanced/"
 #define PATH_TO_NEW_SAMPLES "samples/"
 #define NUM_INPUTS 6
 #define NUM_OUTPUTS 4
-#define NUM_INPUTS_SECOND 400
-#define NUM_OUTPUTS_SECOND 2
-
-
-int getNumOfSamples(char * path){
-	char command[100] = "ls -l ";
-    char size[10];
-    FILE * ls;
-    strcat(command, path);
-    strcat(command, " | wc -l");
-    ls = popen(command, "r");
-    fread(&size, sizeof(char), 10, ls);
-    pclose(ls);
-	return atoi(size)-1;
-}
-
-FANN_EXTERNAL fann_type * getOutput(char * data, struct fann * ann, int in){
-	int	i=0;
-	char * token;
-	FANN_EXTERNAL fann_type input[in];
-    token = strtok(data, " "); //Break up data stream
-	while(token != NULL){
-    	input[i++] = atof(token);
-    	token = strtok(NULL, " ");
-	}
-    return fann_run(ann, input); //run input through network
-}
 
 int main(int argc, char * args[]){
     char data[50]; //Arbitrary length
 	char path[100];
+    char * token;
+    FANN_EXTERNAL fann_type input[NUM_INPUTS];
     FANN_EXTERNAL fann_type * output;
-	FANN_EXTERNAL fann_type * output2;
-    int num_samples, i, fd;
-	struct fann * ann;
-	struct fann * ann2;
-	SRC_DATA src_data;
-	src_data.data_in = malloc(sizeof(float)**4)
+    int num_samples, i;
     
-    if(argc==2){        
+    if(argc==2){
+        
+        char command[100] = "ls -l ";
+        char size[10];
+        FILE * ls;
         sprintf(path, "%s%s/", PATH_TO_SAMPLES, args[1]);
-        num_samples = getNumOfSamples(path);
+        strcat(command, path);
+        strcat(command, " | wc -l");
+
+        ls = popen(command, "r");
+        fread(&size, sizeof(char), 10, ls);
+        num_samples = atoi(size)-1;
+        pclose(ls);
         printf("Sample count: %d\n", num_samples);
     }
 
-    fd = open(PATH_TO_WII_PIPE, 0666);
-    ann = fann_create_from_file(PATH_TO_NETWORK);
-	ann2 = fann_create_from_file(PATH_TO_SECOND_LAYER);
+    int fd = open(PATH_TO_WII_PIPE, 0666);
+    struct fann *ann = fann_create_from_file(PATH_TO_NETWORK);
+
 
 	if(argc==1){
 	    while(1){
 	        if(read(fd,data, 50)!=0){ //Read from pipe
-				output = getOutput(data, ann, NUM_INPUTS);
-				output2 = fann_run(ann2, output);
-				for(i=0; i<NUM_OUTPUTS_SECOND; i++){
-					printf("%f ",output2[i]);
-				}
-				printf("\n");
+        	    i=0;
+    	        token = strtok(data, " "); //Break up data stream
+	            while(token != NULL){
+        	        input[i++] = atof(token);
+    	            token = strtok(NULL, " ");
+	            }
+            	output = fann_run(ann, input); //run input through network
+        	    for(i=0; i<NUM_OUTPUTS; i++){
+    	            printf("%f ",output[i]);
+	            }
+            	printf("\n");
 			}
 		}
 	}else{
 		FILE * sample;
 		FILE * newSamples;
-		char filePath[100]
+		char filePath[100];
 		char filePathNew[100];
 		char message[100];
 		int j, k;
@@ -91,9 +74,16 @@ int main(int argc, char * args[]){
 			newSamples = fopen(filePathNew, "w+");
 
 			while(fscanf(sample, "%s\n", message)!=EOF){
-    	        output = getOutput(message, ann, NUM_INPUTS);
+				j=0;
+	    	    token = strtok(message, " "); //message stream
+		        while(token != NULL){
+        			input[j++] = atof(token);
+    	    	    token = strtok(NULL, " ");
+		        }
+    	        output = fann_run(ann, input); //run input through network
         	    for(k=0; k<NUM_OUTPUTS; k++){
 					fprintf(newSamples, "%f ", output[k]);
+    	        	//printf("%f ",output[k]);
 		        }
 				fprintf(newSamples, "\n");
 			}
@@ -101,7 +91,4 @@ int main(int argc, char * args[]){
 			fclose(sample);	
 		}
 	}
-	return 0;
 }
-
-
