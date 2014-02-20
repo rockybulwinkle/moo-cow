438d0aa4df0803342048fe1aa548b9e1e2112dcc
Chris Hopwood <rockybulwinkle@gmail.com>
2014-02-20 16:43:12 +0000

    a.out is stupid.

--- wiigesturedetection/build.sh
+++ wiigesturedetection/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-gcc wii_capture.c libwiiuse.a  -lm -lbluetooth -o wii_capture
+gcc wii_capture.c libwiiuse.a  -lm -lbluetooth -o wii_capture -o wii_gesture_capture
 #gcc wiigesturedata.c -lm -lfann -o gesture
 gcc wiigesturedata.c libwiiuse.a resample.c -lfann -lm -lsamplerate -lbluetooth
